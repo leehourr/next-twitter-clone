763275bb26c577355f9f78c3baa1f33ae523b3a3
Lee Hour <lhour999@gmail.com>
2023-04-08 11:23:36 +0700

    added layout

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SideBar from "./Layout/SideBar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30  max-w-full">
+        <div className="grid grid-cols-4 h-full">
+          <SideBar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="underline text-white ">twitter clone</div>;
+  return <div className="underline text-white">twitter clone</div>;
 };
 
 export default Home;
